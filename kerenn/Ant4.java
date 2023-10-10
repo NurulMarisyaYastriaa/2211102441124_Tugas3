c09469854db0a54e7519d272a6b52f60c36ea2d1
NurulMarisyaYastriaa <145677540+NurulMarisyaYastriaa@users.noreply.github.com>
2023-10-10 22:18:05 +0800

    Add files via upload

---
+++ kerenn/Ant4.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kutu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ant4 extends Enemies
+{
+    public Ant4(){
+        this.reward = 1;
+        this.speed = 3;
+    }
+}
